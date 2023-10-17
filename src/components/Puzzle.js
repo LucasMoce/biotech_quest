6e22878c9c6d6a20e15d79dc64a4b0bc512dd075
Gabriel Liston <gabriellistondemenek@gmail.com>
2023-10-17 16:05:24 -0300

    mais cadeias

--- src/components/Puzzle.js
+++ src/components/Puzzle.js
@@ -2,14 +2,28 @@ import React, { useState, useRef, useEffect } from 'react';
 
 const Quiz = () => {
   const canvasRef = useRef(null);
-  const [circlePositions, setCirclePositions] = useState([
-    { x: 50, y: 50, label: 'A' },
-    { x: 150, y: 50, label: 'G' },
-    { x: 250, y: 50, label: 'T' },
-    { x: 350, y: 50, label: 'C' },
+
+  const [circleCadeias, setCircleCadeias] = useState([
+    [
+      { x: 50, y: 50, label: 'A' },
+      { x: 150, y: 50, label: 'G' },
+      { x: 250, y: 50, label: 'T' },
+      { x: 350, y: 50, label: 'C' },
+    ],
+    [
+      { x: 50, y: 75, label: 'G' },
+      { x: 150, y: 75, label: 'C' },
+      { x: 250, y: 75, label: 'A' },
+      { x: 350, y: 75, label: 'A' },
+    ],
+    [
+      { x: 50, y: 100, label: 'G' },
+      { x: 150, y: 100, label: 'T' },
+      { x: 250, y: 100, label: 'A' },
+    ],
   ]);
 
-  const [dragging, setDragging] = useState(false);
+  const [dragging, setDragging] = useState({ cadeiaIndex: -1, circleIndex: -1 });
 
   const encaixes = [
     { x: 50, y: 300, label: 'G' },
@@ -45,126 +59,127 @@ const Quiz = () => {
 
       // Desenha o retângulo fino cinza claro
       context.fillStyle = '#f0f0f0';
-      context.fillRect(circlePositions[0].x - circleRadius, circlePositions[0].y - 2, rectangleWidth, 5);
+      context.fillRect(circleCadeias[0][0].x - circleRadius, circleCadeias[0][0].y - 2, rectangleWidth, 5);
 
       // Desenha os círculos vermelhos arrastáveis
-      circlePositions.forEach((circlePosition) => {
-        context.fillStyle = '#ff0000';
-        context.beginPath();
-        context.arc(circlePosition.x, circlePosition.y, circleRadius, 0, 2 * Math.PI);
-        context.fill();
-        context.fillStyle = '#000000';
-        context.font = 'bold 12px Arial';
-        context.fillText(circlePosition.label, circlePosition.x - 5, circlePosition.y + 5);
-
-        // Verifica se o círculo está encaixado e se os labels coincidem
-        const encaixeCorrespondente = encaixes.find(
-          (encaixe) =>
-            encaixe.label === circlePosition.label &&
-            Math.abs(encaixe.x - circlePosition.x) < 5 &&
-            Math.abs(encaixe.y - circlePosition.y) < 5
-        );
-
-        if (encaixeCorrespondente) {
-          // Adiciona um contorno verde ao círculo
-          context.strokeStyle = '#00ff00';
-          context.lineWidth = 2;
+      circleCadeias.forEach((cadeia, cadeiaIndex) => {
+        cadeia.forEach((circlePosition, circleIndex) => {
+          context.fillStyle = '#ff0000';
           context.beginPath();
-          context.arc(
-            circlePosition.x,
-            circlePosition.y,
-            circleRadius + 2, // Aumenta o raio para que o contorno não cubra o círculo vermelho
-            0,
-            2 * Math.PI
+          context.arc(circlePosition.x, circlePosition.y, circleRadius, 0, 2 * Math.PI);
+          context.fill();
+          context.fillStyle = '#000000';
+          context.font = 'bold 12px Arial';
+          context.fillText(circlePosition.label, circlePosition.x - 5, circlePosition.y + 5);
+
+          // Verifica se o círculo está encaixado e se os labels coincidem
+          const encaixeCorrespondente = encaixes.find(
+            (encaixe) =>
+              encaixe.label === circlePosition.label &&
+              Math.abs(encaixe.x - circlePosition.x) < 5 &&
+              Math.abs(encaixe.y - circlePosition.y) < 5
           );
-          context.stroke();
-        }
+
+          if (encaixeCorrespondente) {
+            // Adiciona um contorno verde ao círculo
+            context.strokeStyle = '#00ff00';
+            context.lineWidth = 2;
+            context.beginPath();
+            context.arc(
+              circlePosition.x,
+              circlePosition.y,
+              circleRadius + 2, // Aumenta o raio para que o contorno não cubra o círculo vermelho
+              0,
+              2 * Math.PI
+            );
+            context.stroke();
+          }
+        });
       });
     };
 
     drawShapes();
-  }, [circlePositions]);
+  }, [circleCadeias]);
 
-  const handleMouseDown = (e) => {
-    const rect = canvasRef.current.getBoundingClientRect();
-    const mouseX = e.clientX - rect.left;
-    const mouseY = e.clientY - rect.top;
-
-    // Verifica qual círculo foi clicado
-    const clickedCircleIndex = circlePositions.findIndex((circlePosition) => {
-      const distance = Math.sqrt(
-        (mouseX - circlePosition.x) ** 2 + (mouseY - circlePosition.y) ** 2
-      );
-      return distance <= 25;
-    });
-
-    if (clickedCircleIndex !== -1) {
-      setDragging(clickedCircleIndex);
-    }
+  const handleMouseDown = (cadeiaIndex, circleIndex) => {
+    setDragging({ cadeiaIndex, circleIndex });
   };
 
   const handleMouseUp = () => {
-    if (dragging !== false) {
-      // Verifica se todos os círculos têm um local disponível para encaixe
-      const todosPodemEncaixar = circlePositions.every((circlePosition, index) => {
-        const encaixeEncontrado = encaixes.find((encaixe, encaixeIndex) => {
-          const distance = Math.sqrt(
-            (circlePosition.x - encaixe.x) ** 2 + (circlePosition.y - encaixe.y) ** 2
-          );
-          return distance < 30 && !circleIsOccupying(encaixeIndex);
-        });
-        return encaixeEncontrado;
+  if (dragging.cadeiaIndex !== -1 && dragging.circleIndex !== -1) {
+    // Verifica se todos os círculos da mesma cadeia têm um local disponível para encaixe
+    const todosPodemEncaixar = circleCadeias[dragging.cadeiaIndex].every((circlePosition, circleIndex) => {
+      const encaixeEncontrado = encaixes.find((encaixe, encaixeIndex) => {
+        const distance = Math.sqrt(
+          (circlePosition.x - encaixe.x) ** 2 + (circlePosition.y - encaixe.y) ** 2
+        );
+        return distance < 30 && !circleIsOccupying(encaixeIndex, dragging.cadeiaIndex, circleIndex);
       });
+      return encaixeEncontrado;
+    });
 
-      // Se todos podem encaixar, ajusta a posição para o encaixe
-      if (todosPodemEncaixar) {
-        const novasPosicoes = circlePositions.map((circlePosition, index) => {
-          const encaixeEncontrado = encaixes.find((encaixe, encaixeIndex) => {
-            const distance = Math.sqrt(
-              (circlePosition.x - encaixe.x) ** 2 + (circlePosition.y - encaixe.y) ** 2
-            );
-            return distance < 30 && !circleIsOccupying(encaixeIndex);
-          });
+    // Se todos podem encaixar, ajusta a posição para o encaixe
+    if (todosPodemEncaixar) {
+      const novasPosicoes = circleCadeias.map((cadeia, cadeiaIndex) =>
+        cadeiaIndex === dragging.cadeiaIndex
+          ? cadeia.map((circlePosition, circleIndex) => {
+              const encaixeEncontrado = encaixes.find((encaixe, encaixeIndex) => {
+                const distance = Math.sqrt(
+                  (circlePosition.x - encaixe.x) ** 2 + (circlePosition.y - encaixe.y) ** 2
+                );
+                return distance < 30 && !circleIsOccupying(encaixeIndex, dragging.cadeiaIndex, circleIndex);
+              });
 
-          return encaixeEncontrado
-            ? { ...circlePosition, x: encaixeEncontrado.x, y: encaixeEncontrado.y }
-            : circlePosition;
-        });
+              return encaixeEncontrado
+                ? { ...circlePosition, x: encaixeEncontrado.x, y: encaixeEncontrado.y }
+                : circlePosition;
+            })
+          : cadeia
+      );
 
-        setCirclePositions(novasPosicoes);
-      }
-
-      setDragging(false);
+      setCircleCadeias(novasPosicoes);
     }
-  };
 
-  const handleMouseMove = (e) => {
-    if (dragging !== false) {
+    setDragging({ cadeiaIndex: -1, circleIndex: -1 });
+  }
+};
+
+
+  const handleMouseMove = (cadeiaIndex, circleIndex, e) => {
+    if (dragging.cadeiaIndex !== -1 && dragging.circleIndex !== -1) {
       const rect = canvasRef.current.getBoundingClientRect();
       const mouseX = e.clientX - rect.left;
       const mouseY = e.clientY - rect.top;
 
       // Calcula o deslocamento do círculo em relação à sua posição original
-      const offsetX = mouseX - circlePositions[dragging].x;
-      const offsetY = mouseY - circlePositions[dragging].y;
+      const offsetX = mouseX - circleCadeias[dragging.cadeiaIndex][dragging.circleIndex].x;
+      const offsetY = mouseY - circleCadeias[dragging.cadeiaIndex][dragging.circleIndex].y;
 
-      // Atualiza a posição de todos os círculos mantendo as posições relativas
-      setCirclePositions((prevPositions) =>
-        prevPositions.map((position, index) => ({
-          ...position,
-          x: position.x + offsetX,
-          y: position.y + offsetY,
-        }))
+      // Atualiza a posição do círculo arrastado mantendo as posições relativas
+      setCircleCadeias((prevCadeias) =>
+        prevCadeias.map((cadeia, cIndex) =>
+          cIndex === dragging.cadeiaIndex
+            ? cadeia.map((circlePosition, ciIndex) => ({
+                ...circlePosition,
+                x: circlePosition.x + offsetX,
+                y: circlePosition.y + offsetY,
+              }))
+            : cadeia
+        )
       );
     }
   };
 
-  const circleIsOccupying = (encaixeIndex) => {
-    return circlePositions.some(
-      (position, index) =>
-        index !== dragging &&
-        position.x === encaixes[encaixeIndex].x &&
-        position.y === encaixes[encaixeIndex].y
+  const circleIsOccupying = (encaixeIndex, cadeiaIndex, circleIndex) => {
+    return circleCadeias.some(
+      (cadeia, cIndex) =>
+        cIndex !== cadeiaIndex &&
+        cadeia.some(
+          (position, index) =>
+            index !== circleIndex &&
+            position.x === encaixes[encaixeIndex].x &&
+            position.y === encaixes[encaixeIndex].y
+        )
     );
   };
 
@@ -174,9 +189,27 @@ const Quiz = () => {
         ref={canvasRef}
         width={window.innerWidth}
         height={window.innerHeight - 50}
-        onMouseDown={handleMouseDown}
+        onMouseDown={(e) => {
+          const rect = canvasRef.current.getBoundingClientRect();
+          const mouseX = e.clientX - rect.left;
+          const mouseY = e.clientY - rect.top;
+
+          // Verifica em qual cadeia e círculo foi clicado
+          circleCadeias.forEach((cadeia, cadeiaIndex) => {
+            cadeia.forEach((circlePosition, circleIndex) => {
+              const distance = Math.sqrt(
+                (mouseX - circlePosition.x) ** 2 + (mouseY - circlePosition.y) ** 2
+              );
+              if (distance <= 25) {
+                handleMouseDown(cadeiaIndex, circleIndex);
+              }
+            });
+          });
+        }}
         onMouseUp={handleMouseUp}
-        onMouseMove={handleMouseMove}
+        onMouseMove={(e) => {
+          handleMouseMove(dragging.cadeiaIndex, dragging.circleIndex, e);
+        }}
       />
     </div>
   );
