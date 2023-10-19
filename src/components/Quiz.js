4c9b5732f057f557544407df2a576afe3b659c86
grutzmann9@gmail.com <grutzmann9@gmail.com>
2023-10-18 23:45:02 -0300

    quiz pronto feio

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,57 +3,98 @@ import { useState } from 'react'
 
 const Quiz = () => {
 
-  const [questions, setQuestions] = useState([{
-    q: "Qual o nome da primeira ovelha clonada?",
-    a: [{ text: "Tereza", isCorrect: false },
-    { text: "Chiquinha", isCorrect: false },
-    { text: "Dolly", isCorrect: true },
-    { text: "Maria de Lurdes Conceição", isCorrect: false }
-    ]
- 
-},
-{
-    q: "What is the capital of Thailand?",
-    a: [{ text: "Lampang", isCorrect: false, isSelected: false },
-    { text: "Phuket", isCorrect: false },
-    { text: "Ayutthaya", isCorrect: false },
-    { text: "Bangkok", isCorrect: true }
-    ]
- 
-},
-{
-    q: "What is the capital of Gujarat",
-    a: [{ text: "Surat", isCorrect: false },
-    { text: "Vadodara", isCorrect: false },
-    { text: "Gandhinagar", isCorrect: true },
-    { text: "Rajkot", isCorrect: false }
-    ]
- 
-}
- 
-]);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [atualQuestion, setAtualQuestion] = useState(0);
+  const [pontos, setPontos] = useState(0);
+  const [acabou, setAcabou] = useState(false);
 
-  const teste = () => {
-    let a = "";
-    for (let i = 0; i< questions[0].a.length; i+= 1){
-        a += questions[0].a[i].text 
-    }
-    return a
+  const [quizData, setQuizData] = useState([
+    {
+      question: "Qual o nome da primeira ovelhada clonada?",
+      options: ['Dolly', "Maria Chiquinha", "Renata", "Mariana Fonseca"],
+      answer: "Dolly"
+    },
+    {
+      question: "Qual o nome da segunda ovelhada clonada?",
+      options: ['Dolly', "Maria Chiquinha", "Renata", "Mariana Fonseca"],
+      answer: "Dolly"
+    },
+    {
+      question: "Qual o nome da terceira ovelhada clonada?",
+      options: ['Dolly', "Maria Chiquinha", "Renata", "Mariana Fonseca"],
+      answer: "Dolly"
+    },
+    {
+      question: "Qual o nome da quarta ovelhada clonada?",
+      options: ['Dolly', "Maria Chiquinha", "Renata", "Mariana Fonseca"],
+      answer: "Dolly"
+    },
+    {
+      question: "Qual o nome da quinta ovelhada clonada?",
+      options: ['Dolly', "Maria Chiquinha", "Renata", "Mariana Fonseca"],
+      answer: "Dolly"
+    },
+  ])
+
+  const handleOptionChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  const renderOptions = (options) =>
+  {
+    return options.map((option, index) => (
+      <>
+        <label htmlFor={option} className='mt-3'>
+        <input 
+        type="radio"
+        name="quizOption"
+        value={option}
+        checked={selectedOption == option}
+        onChange={handleOptionChange} 
+        className='me-3'
+        id={option}
+        />
+        {option}
+        </label>
+        <br />
+      </>
+    ))
   }
+
+  const processAnswer = (()=> {
+    if (selectedOption == quizData[atualQuestion].answer)
+    {
+      setPontos(pontos + 1);
+      if (atualQuestion < quizData.length -1) {
+        setAtualQuestion(atualQuestion + 1);
+      } else {setAcabou(true)}
+    } else {
+      if (atualQuestion < quizData.length -1) {
+        setAtualQuestion(atualQuestion + 1);
+      } else {setAcabou(true)}
+    }
+  })
 
   return (
     <div>
-      <div 
-      className='container text-center mt-5 pt-5 rounded border border-primary border-2 bg-light'
-      style={{height:`600px`}}
-      >
-        <h1 className='text-dark'>Quiz da Biotecnologia</h1>
-        <h3 className='mt-5'>{questions[0].q}</h3>
-        <p>{questions[0].a.map((option, index) => (
-          <p key={index}>{option.text}</p>
-        ))}</p>
+      <div className='container mt-5 pt-5 rounded border border-primary border-5 bg-light'style={{height:`600px`}}>
+        <h1 className='text-dark text-center'>Quiz da Biotecnologia</h1>
+        <div className='mx-auto' style={{width:`500px`}}>
+          {acabou ? 
+            <>
+              <h3 className='text-center mt-5'>Sua pontuação: {pontos}/{quizData.length}</h3>
+            </>
+          :
+            <>
+              <h4 className='mt-4'>{quizData[atualQuestion].question}</h4>
+              {renderOptions(quizData[atualQuestion].options)}
+              <button className='btn btn-primary mt-5 translate-middle' style={{marginLeft:`50%`, marginRight:`50%`}} onClick={processAnswer}>Enviar</button>
+              <h4 className='text-center'>{atualQuestion +1}/{quizData.length}</h4>
+            </>
+          }         
+        </div>       
       </div>
-        
+      
     </div>
   )
 }
