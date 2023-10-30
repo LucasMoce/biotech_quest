c8e431daf0553d6433a500391ed14678587ff437
grutzmann9@gmail.com <grutzmann9@gmail.com>
2023-10-30 18:37:32 -0300

    foto do puzzle

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link, useMatch, useResolvedPath, } from "react-router-dom";
 import quiz from './quiz.png';
+import puzzle from './puzzle.png';
 
 const Home = () => {
   return (
@@ -15,14 +16,15 @@ const Home = () => {
                     </div>
                     <br />
                     <div className="border border-dark border-3 position-relative rounded" style={{backgroundColor:`darkred`, height:`200px`}}>
-                        <h2 className='text-light position-absolute top-50 start-50 translate-middle'>puzzle</h2>
-                        <button className='btn btn-light position-absolute bottom-0 start-50 translate-middle'><Link to="/puzzle" className="nav-link">Iniciar</Link></button>
+                        <h2 className='text-light position-absolute top-50 start-50 translate-middle'>Arrume o DNA</h2>
+                        <button className='btn btn-light position-absolute bottom-0 start-50 translate-middle'><Link to="/DNAGame" className="nav-link">Iniciar</Link></button>
                     </div>
                 </div>
                 <div className='col me-5'>
-                <div className="border border-dark border-3 position-relative rounded" style={{backgroundColor:`darkred`, height:`200px`}}>
-                        <h2 className='text-light position-absolute top-50 start-50 translate-middle'>puzzle</h2>
-                        <button className='btn btn-light position-absolute bottom-0 start-50 translate-middle'><Link to="/puzzle" className="nav-link">Iniciar</Link></button>
+                <div className="border border-dark border-3 position-relative rounded" style={{height:`200px`}}>
+                        <Link to="/puzzle" className="nav-link">
+                        <img src={puzzle} alt="" />
+                        </Link>
                     </div>
                     <br />
                     <div className="border border-dark border-3 position-relative rounded" style={{backgroundColor:`darkred`, height:`200px`}}>
